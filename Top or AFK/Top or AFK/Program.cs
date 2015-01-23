67a152d902c07e1dbfeb34e569f44d82b35053bb
TehBlaxxor <stefandac@gmail.com>
2015-01-23 20:07:34 +0200

    TOPORAFKINITIAL

---
+++ Top or AFK/Top or AFK/Program.cs
@@ -0,0 +1,33 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using TopOrAFK.Champions;
+
+namespace TopOrAFK
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string ChampionSwitch = ObjectManager.Player.ChampionName.ToLowerInvariant();
+
+            switch (ChampionSwitch)
+            {
+                case "aatrox":
+                    new Aatrox();
+                    Game.PrintChat("<font color='#FF00BF'>[TOPORAFK] : Aatrox | By TehBlaxxor</font>", ChampionSwitch);
+                    break;
+
+                default:
+                    Game.PrintChat("[TOPORAFK] Failed to load assembly for champion : NOT SUPPORTED!", ChampionSwitch);
+                    break;
+            }
+
+
+        }
+    }
+}
---
+++ Top or AFK/Top or AFK/TO DO LIST.cs
@@ -0,0 +1,55 @@
+﻿/* TOP OR AFK -- TO DO LIST
+===============> CHAMPIONS <===============
+ * Aatrox
+ * Annie
+ * Jax
+ * Garen
+ * Ryze
+ * Teemo
+ * Udyr
+ * Vayne
+ * Xin Zhao
+ * Zed
+ * Darius
+===============> BUG FIXES <===============
+
+
+===============> SUGGESTIONS <===============
+
+
+===============> WIP <===============
+ * Aatrox Plugin
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
