1cedb3b42ad917a6385e250a38fc3875ce5a9861
TehBlaxxor <stefandac@gmail.com>
2015-04-17 15:53:14 +0300

    malzahar

---
+++ TehMalzahar/TehMalzahar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace TehMalzahar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            if (ObjectManager.Player.ChampionName == "Malzahar")
+            {
+                new Malzahar();
+                Game.PrintChat("");
+            }
+            else Game.PrintChat("Failed to load Malzahar for the selected champion: " + ObjectManager.Player.ChampionName);
+        }
+    }
+}
