3823820a6b397ce3e33aa2d226e8087d0496fcdd
TehBlaxxor <stefandac@gmail.com>
2015-04-30 20:45:33 +0300

    hi

--- WASD Controls/WASD Controls/Program.cs
+++ WASD Controls/WASD Controls/Program.cs
@@ -14,10 +14,27 @@ namespace WASD_Controls
     {
         public static Menu Config;
         public static Obj_AI_Hero Player = ObjectManager.Player;
+        public static bool Enabled = true;
 
         static void Main(string[] args)
         {
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+            Game.OnInput += Game_OnInput;
+        }
+
+        static void Game_OnInput(GameInputEventArgs args)
+        {
+            switch (args.Input)
+            {
+                case "#disable":
+                    Enabled = false;
+                    break;
+                case "#enable":
+                    Enabled = true;
+                    break;
+                default:
+                    return;
+            }
         }
 
         static void Game_OnGameLoad(EventArgs args)
@@ -28,6 +45,7 @@ namespace WASD_Controls
             Config.AddItem(new MenuItem("a", "Left").SetValue(new KeyBind('G', KeyBindType.Press)));
             Config.AddItem(new MenuItem("s", "Down").SetValue(new KeyBind('H', KeyBindType.Press)));
             Config.AddItem(new MenuItem("d", "Right").SetValue(new KeyBind('J', KeyBindType.Press)));
+            Config.AddItem(new MenuItem("autodisable", "Automatically Disable Spells").SetValue(true));
             Config.AddItem(new MenuItem("summoner", "Disable D Summoner Spell").SetValue(false));
             Config.AddItem(new MenuItem("summoner2", "Disable F Summoner Spell").SetValue(false));
             Config.AddItem(new MenuItem("summoner3", "Disable Q").SetValue(false));
@@ -42,10 +60,26 @@ namespace WASD_Controls
             Spellbook.OnCastSpell += Spellbook_OnCastSpell;
         }
 
+        public static bool Check(string menuitem)
+        {
+            return (Config.Item(menuitem).GetValue<KeyBind>().Key == 'Q'
+                || Config.Item(menuitem).GetValue<KeyBind>().Key == 'W'
+                || Config.Item(menuitem).GetValue<KeyBind>().Key == 'E'
+                || Config.Item(menuitem).GetValue<KeyBind>().Key == 'R'
+                || Config.Item(menuitem).GetValue<KeyBind>().Key == 'D'
+                || Config.Item(menuitem).GetValue<KeyBind>().Key == 'F') && Config.Item(menuitem).GetValue<KeyBind>().Active;
+        }
+
         static void Spellbook_OnCastSpell(Spellbook sender, SpellbookCastSpellEventArgs args)
         {
-            if (!sender.Owner.IsMe)
+            if (!sender.Owner.IsMe || Enabled == false)
                 return;
+
+            if (Config.Item("autodisable").GetValue<bool>())
+            {
+                if (Check("w") || Check("a") || Check("s") || Check("d"))
+                    args.Process = false;
+            }
 
             if (args.Slot == SpellSlot.Summoner1 && Config.Item("summoner").GetValue<bool>())
             {
@@ -75,6 +109,9 @@ namespace WASD_Controls
 
         static void Game_OnUpdate(EventArgs args)
         {
+            if (Enabled == false)
+                return;
+
             if (Config.Item("w").GetValue<KeyBind>().Active 
                 && Config.Item("a").GetValue<KeyBind>().Active)
             {
