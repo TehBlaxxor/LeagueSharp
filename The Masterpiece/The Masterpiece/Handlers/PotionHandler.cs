c80e1d01f7b53d428796690fca1b959398c1fa13
livetorise <tyler.martel@live.com>
2015-05-02 01:34:11 -0700

    ReWorked Working Potion Manager Now
    Working potions! Has cookies! Flask and HP and MP pots!

--- The Masterpiece/The Masterpiece/Handlers/PotionHandler.cs
+++ The Masterpiece/The Masterpiece/Handlers/PotionHandler.cs
@@ -1,9 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
@@ -14,8 +13,13 @@ namespace The_Masterpiece.Handlers
     {
         private const string HP_POT_NAME = "RegenerationPotion";
         private const string MP_POT_NAME = "FlaskOfCrystalWater";
+        private const string MP2_POT_NAME = "ItemCrystalFlask";
+        private const string MP3_POT_NAME = "ItemMiniRegenPotion";
         private const int HP_ID = 2003;
         private const int MP_ID = 2004;
+        private const int HP_ID2 = 2010;
+        private const int MP_ID3 = 2041;
+
         private static Menu menu;
 
         public void Load(Menu config)
@@ -28,45 +32,55 @@ namespace The_Masterpiece.Handlers
 
             menu = config;
 
-            Game.OnUpdate += OnUpdate;
+            Game.OnUpdate += OnGameUpdate;
         }
 
-        private static void OnUpdate(EventArgs args)
+        private void OnGameUpdate(EventArgs args)
         {
-            var useHp = menu.Item("themp.healthpots.active").GetValue<bool>();
-            var useMp = menu.Item("themp,manapots.active").GetValue<bool>();
-            if (ObjectManager.Player.IsRecalling() || ObjectManager.Player.InFountain() || ObjectManager.Player.InShop())
-            {
+            if (ObjectManager.Player.HasBuff("Recall") || ObjectManager.Player.InFountain() && ObjectManager.Player.InShop())
                 return;
-            }
 
-            if (useHp && ObjectManager.Player.HealthPercent <= menu.Item("themp.healthpots.health").GetValue<Slider>().Value &&
-                !HasHealthPotActive())
+            try
             {
-                if (Items.CanUseItem(HP_ID) && Items.HasItem(HP_ID))
+                if (menu.Item("themp.healthpots.active").GetValue<bool>())
                 {
-                    Items.UseItem(HP_ID);
+                    if (ObjectManager.Player.HealthPercentage() <= menu.Item("themp.healthpots.health").GetValue<Slider>().Value)
+                    {
+                        if (Items.CanUseItem(HP_ID) && Items.HasItem(HP_ID))
+                        {
+                            Items.UseItem(HP_ID);
+                        }
+                        if (Items.CanUseItem(HP_ID2) && Items.HasItem(HP_ID2))
+                        {
+                            Items.UseItem(HP_ID2);
+                        }
+                        if (Items.CanUseItem(MP_ID3) && Items.HasItem(MP_ID3))
+                        {
+                            Items.UseItem(MP_ID3);
+                        }
+                    }
+                }
+                if (menu.Item("themp.manapots.active").GetValue<bool>())
+                {
+                    if (ObjectManager.Player.ManaPercentage() <= menu.Item("themp.manapots.mana").GetValue<Slider>().Value)
+                    {
+                        Game.PrintChat("Manapotsareworking");
+                        if (Items.CanUseItem(MP_ID) && Items.HasItem(MP_ID))
+                        {
+                            Items.UseItem(MP_ID);
+                        }
+                        if (Items.CanUseItem(MP_ID3) && Items.HasItem(MP_ID3))
+                        {
+                            Items.UseItem(MP_ID3);
+                        }
+                    }
                 }
             }
 
-            if (!useMp ||
-                !(ObjectManager.Player.ManaPercent <= menu.Item("themp.manapots.mana").GetValue<Slider>().Value) ||
-                HasMannaPutActive()) return;
-
-            if (Items.CanUseItem(MP_ID) && Items.HasItem(MP_ID))
+            catch (Exception)
             {
-                Items.UseItem(MP_ID);
+                Game.PrintChat("Error using potions D:");
             }
-        }
-
-        private static bool HasHealthPotActive()
-        {
-            return ObjectManager.Player.HasBuff(HP_POT_NAME, true);
-        }
-
-        private static bool HasMannaPutActive()
-        {
-            return ObjectManager.Player.HasBuff(MP_POT_NAME, true);
         }
     }
 }
